293916f09c477c8fb1fcf686b9e2fa063ef5640b
HeidiKorp <23059951+HeidiKorp@users.noreply.github.com>
2017-09-30 23:40:00 +0300

    Update TestWeather.java

--- src/main/java/TestWeather.java
+++ src/main/java/TestWeather.java
@@ -1,12 +1,22 @@
+package weatherProgram;
+
+import org.junit.Assert;
 import org.junit.Test;
+import validator.Validator;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import static java.util.Locale.UK;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestWeather {
+
+    private Validator validator = new Validator("Tallinn", "EE", "celsius");
+    private WeatherRequest request = new WeatherRequest("Tallinn", "EE", "celsius");
+    private WeatherCollection collection = new WeatherCollection();
+    private WeatherReport report = collection.getCurrentWeather(request);
 
     @Test
     public void testSuccessfulConnectionToAPI() {
@@ -24,48 +34,158 @@ public class TestWeather {
     @Test
     public void testRequestedAndReceivedNameOfCity() {
         try {
-            WeatherRequest request = new WeatherRequest("London", UK);
-            WeatherCollection repository = new WeatherCollection();
-            WeatherReport report = repository.getCurrentWeather(request);
-            assertEquals(request.getCity(), report.getCityName());
-        } catch (RuntimeException e) {
+            assert report.getCityName().equals(request.getCity());
+        } catch (NullPointerException e) {
             e.printStackTrace();
+            fail();
         }
     }
 
     @Test
     public void testRequestedAndReceivedCountry() {
         try {
-            WeatherRequest request = new WeatherRequest("London", UK);
-            WeatherCollection repository = new WeatherCollection();
-            WeatherReport report = repository.getCurrentWeather(request);
-            assertEquals(request.getCountryAlphaCode(), report.getCountry());
-        } catch (RuntimeException e) {
+            assert report.getCountry().equals(request.getCountryAlphaCode());
+        } catch (NullPointerException e) {
             e.printStackTrace();
+            fail();
         }
     }
 
     @Test
-    public void testLowestTemperatureIsSmallerThanHighestTemp() {
+    public void testValidateTempLowestHighestEqual() {
+        validator.validateLowestTempIsSmallerThanHighestTemp(55, 55);
+    }
+
+    @Test
+    public void testValidateTempHighestSmallerThanLowest() {
+        validator.validateLowestTempIsSmallerThanHighestTemp(3, 55);
+    }
+
+    @Test
+    public void testValidateTempHighestBiggerThanLowest() {
+        validator.validateLowestTempIsSmallerThanHighestTemp(33, 5);
+    }
+
+    @Test
+    public void testValidateTempCelsiusTooHigh() {
+        validator.validateTemperatureIsInCorrectRange("celsius", 300, 2);
+    }
+
+    @Test
+    public void testValidateTempCelsiusTooLow() {
+        validator.validateTemperatureIsInCorrectRange("celsius", 30, -1000);
+    }
+
+    @Test
+    public void testValidateTempCelsiusCorrect() {
+        validator.validateTemperatureIsInCorrectRange("celsius", 30, 30);
+    }
+
+    @Test
+    public void testValidateTempCelsiusLowestTempHigherThanHighest() {
+        validator.validateTemperatureIsInCorrectRange("celsius", 2, 30);
+    }
+
+    @Test
+    public void testValidateTempKelvinTooHigh() {
+        validator.validateTemperatureIsInCorrectRange("kelvin", 9000, 30);
+    }
+
+    @Test
+    public void testValidateTempKelvinTooLow() {
+        validator.validateTemperatureIsInCorrectRange("kelvin", 100, -200);
+    }
+
+    @Test
+    public void testValidateKelvinCorrect() {
+        validator.validateTemperatureIsInCorrectRange("kelvin", 280, 276);
+    }
+
+    @Test
+    public void testValidateTempKelvinTooHighTooLow() {
+        validator.validateTemperatureIsInCorrectRange("kelvin", 9000, -90);
+    }
+
+    @Test
+    public void testValidateTempFahrenheitTooHigh() {
+        validator.validateTemperatureIsInCorrectRange("fahrenheit", 9000, 40);
+    }
+
+    @Test
+    public void testValidateTempFahrenheitTooLow() {
+        validator.validateTemperatureIsInCorrectRange("fahrenheit", 100, -600);
+    }
+
+    @Test
+    public void testValidateTempFahrenheitCorrect() {
+        validator.validateTemperatureIsInCorrectRange("fahrenheit", 100, 100);
+    }
+
+    @Test
+    public void testValidateLatitudeTooBig() {
+        validator.validateCoordinateRange(100, 20);
+    }
+
+    @Test
+    public void testValidateLatitudeTooSmall() {
+        validator.validateCoordinateRange(-100, 20);
+    }
+
+    @Test
+    public void testValidateLongitudeTooBig() {
+        validator.validateCoordinateRange(60, 200);
+    }
+
+    @Test
+    public void testValidateLongitudeTooSmall() {
+        validator.validateCoordinateRange(60, -200);
+    }
+
+    @Test
+    public void testValidateLatitudeAndLongitudeCorrect() {
+        validator.validateCoordinateRange(90, 180);
+    }
+
+    @Test
+    public void testCoordinatesFormatTwoDigits() {
+        validator.validateCoordinateFormat("33:92");
+    }
+
+    @Test
+    public void testCoordinatesFormatOneDigit() {
+        validator.validateCoordinateFormat("2:2");
+    }
+
+    @Test
+    public void testCoordinatesFormatCommaSeparator() {
+        validator.validateCoordinateFormat("22,33");
+    }
+
+    @Test
+    public void testCoordinatesFormatSpaceSeparator() {
+        validator.validateCoordinateFormat("33 44");
+    }
+
+    @Test
+    public void testCanGetCityNameFromResponse() {
+        assert report.getCityName().equals(request.getCity());
+    }
+
+    @Test
+    public void testCanGetHighestTempFromResponse() {
         try {
-            WeatherRequest request = new WeatherRequest("London", UK);
-            WeatherCollection repository = new WeatherCollection();
-            WeatherReport report = repository.getCurrentWeather(request);
-            assert report.getLowestTemperature() <= report.getHighestTemperature();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+            assert report.getHighestTemperature() != 0;
+        } catch (AssertionError e) {
+            fail("Cannot find highest temperature.");
         }
     }
 
     @Test
-    public void testIfCoordinatesInCorrectFormat() {
+    public void testCanGetLowestTempFromReport() {
         try {
-            WeatherRequest request = new WeatherRequest("London", UK);
-            WeatherCollection repository = new WeatherCollection();
-            WeatherReport report = repository.getCurrentWeather(request);
-            assert report.getCoordinates().matches("\\d{3}:\\d{3}");
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+            assert report.getLowestTemperature() != 0;
+        } catch (AssertionError e) {
+            fail("Cannot find lowest temperature.");
         }
     }
 
